b0af643aa089ab6f97b5281942c42d52ee079ed2
Lucien Boudy <luboudy@gmail.com>
2024-09-07 10:28:50 +0200

    chore: deno fmt

--- templates/pwa-game/public/sw.js
+++ templates/pwa-game/public/sw.js
@@ -3,13 +3,13 @@ const cacheName = "template-store-v1";
 // Installing Service Worker
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(["/index.html"]))
+    caches.open(cacheName).then((cache) => cache.addAll(["/index.html"])),
   );
 });
 
 // Fetching content using Service Worker
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request)),
   );
 });
